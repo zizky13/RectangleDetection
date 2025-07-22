dbee9bb1180592d0cc499ff3f6417e6c77fc5740
zizky13 <zikar.nurizky@gmail.com>
2025-07-22 18:39:35 +0700

    Initial Commit

---
+++ RectangleDetection/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RectangleDetection
+//
+//  Created by Zikar Nurizky on 22/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RectangleDetection/RectangleDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  RectangleDetectionApp.swift
+//  RectangleDetection
+//
+//  Created by Zikar Nurizky on 22/07/25.
+//
+
+import SwiftUI
+
+@main
+struct RectangleDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
